757a3b58a0d948e32cd31ac0b7b196c21b54d6cd
Adson Martins <adson.martins982@gmail.com>
2022-07-30 16:43:49 -0300

    feat: new improvements

---
+++ assets/js/active.js
@@ -0,0 +1,31 @@
+const form = document.querySelector("form");
+const keyInput = document.querySelector("input#activeKey");
+const submitButton = document.querySelector("button[type=submit]");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const key = keyInput.value;
+  const url = `https://api.hydranetwork.org/auth/?type=activatekey&mdkey=${key}`;
+
+  submitButton.disabled = true;
+  submitButton.innerHTML = `<div class="spinner-border spinner-border-sm" role="status"><span class="visually-hidden">Loading...</span></div>`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      const text = data.status
+        ? `${data.message} (${data.until})`
+        : data.message;
+
+      Toastify({
+        text,
+        className: data.status ? "success" : "warning",
+        duration: 5000,
+        style: {
+          background: "var(--purple)",
+        },
+      }).showToast();
+
+      submitButton.innerHTML = "Active";
+      submitButton.disabled = false;
+    });
+});
